e955b5f2f3f28d8d60ad9c80e737e125396c1b1c
Peter Tsegwi <tsegwipeter@gmail.com>
2025-04-09 14:47:38 +0100

    added oraganisation permission types

--- apistate_common/models/permission.py
+++ apistate_common/models/permission.py
@@ -1,20 +1,20 @@
-from pydantic import BaseModel
-from typing import Optional
+from mongoengine import StringField
 from ..enums.permission_action import PermissionAction
 from ..enums.resource_type import OrganisationResourceType, OrganisationalUnitResourceType
 from .base import BaseEmbeddedDocument
+
 
 class OrganisationPermission(BaseEmbeddedDocument):
     """
     Embedded model for Permission with action and resource fields of organisation
     """
-    action: PermissionAction
-    resource_type: OrganisationResourceType
+    action = StringField(required=True, choices=[action.value for action in PermissionAction])
+    resource_type = StringField(required=True, choices=[res_type.value for res_type in OrganisationResourceType])
 
 
 class OrganisationalUnitPermission(BaseEmbeddedDocument):
     """
     Embedded model for Permission with action and resource fields of organisational unit
     """
-    action: PermissionAction
-    resource_type: OrganisationalUnitResourceType
+    action = StringField(required=True, choices=[action.value for action in PermissionAction])
+    resource_type = StringField(required=True, choices=[res_type.value for res_type in OrganisationalUnitResourceType])
