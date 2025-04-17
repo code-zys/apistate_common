81b48a957cc0096f837d20fee308418760d6413b
Peter Tsegwi <tsegwipeter@gmail.com>
2025-04-17 06:49:49 +0100

    updated ou ability

--- apistate_common/models/user_organisational_unit_ability.py
+++ apistate_common/models/user_organisational_unit_ability.py
@@ -5,6 +5,7 @@ from .permission import OrganisationalUnitPermission
 class UserOrganisationalUnitAbility(BaseOrganisationDocument):
     user = ReferenceField('User', required=True)
     permissions = ListField(EmbeddedDocumentField(OrganisationalUnitPermission), default=[], required=True)
+    organisational_unit = ReferenceField('OrganisationalUnit', required=True)
     
     meta = {
         'collection': 'user_organisation_unit_abilities',
