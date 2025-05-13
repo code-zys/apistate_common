76037c3b182f8065b00813071d3b555ebaf4155e
Peter Tsegwi <tsegwipeter@gmail.com>
2025-05-13 13:32:48 +0100

    added health config

---
+++ apistate_common/mappers/health_config.py
@@ -0,0 +1,28 @@
+from apistate_common.models.health_config import HealthCheckConfig
+from apistate_common.dtos.health_config import HealthCheckConfigResponseDto, RequestBodyCheckDto
+from bson import ObjectId
+
+class HealthCheckConfigMapper:
+    @staticmethod
+    def to_dto(config: HealthCheckConfig) -> HealthCheckConfigResponseDto:
+        """Convert a HealthCheckConfig model to a DTO.
+        
+        Args:
+            config: The HealthCheckConfig model to convert
+            
+        Returns:
+            HealthCheckConfigResponseDto: The converted DTO
+        """
+        return HealthCheckConfigResponseDto(
+            id=str(config.id),
+            endpoint_id=str(config.endpoint.id) if config.endpoint else None,
+            cron=config.cron,
+            status_code=config.status_code,
+            body_checks=[
+                RequestBodyCheckDto(
+                    path=check.path,
+                    expected_value=check.expected_value
+                ) for check in config.body_checks
+            ] if config.body_checks else [],
+            api_id=str(config.api.id)
+        )
