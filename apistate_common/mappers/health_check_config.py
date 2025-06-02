0055972210023cf214bf892dd566af37776f44de
Peter Tsegwi <tsegwipeter@gmail.com>
2025-06-02 10:17:03 +0100

    update health check config mapper

--- apistate_common/mappers/health_check_config.py
+++ apistate_common/mappers/health_check_config.py
@@ -15,14 +15,6 @@ class HealthCheckConfigMapper:
             id=str(config.id),
             endpoint_id=endpoint,
             cron=config.cron,
-            # status_code=config.status_code,
-            body_checks=[
-                RequestBodyCheckDto(
-                    path=check.path,
-                    value=check.value,
-                    operator=check.operator
-                ) for check in config.body_checks
-            ],
             api_id=str(config.api.id),
             created_at=config.created_at,
             path_params_data=config.path_params_data,
